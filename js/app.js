ec3d52b996e115604bbdb9b734c24b626e0c104e
jrrubalc <jason.ray.rubalcaba@gmail.com>
2017-01-10 13:41:18 -0800

    added yelp function

--- js/app.js
+++ js/app.js
@@ -181,10 +181,14 @@ var ViewModel = function() {
         if (infowindow.marker != marker) {
             infowindow.setContent('');
             infowindow.marker = marker;
-            infowindow.setContent('<div>Hello</div>');
+            infowindow.setContent(`
+                <h4>${marker.creamery}</h4>
+                <h6>${marker.address}</h6>
+                <p id="text">Rating on <a id="yelp-url">yelp</a>:
+                <img id="yelp"></p></div>
+                `);
+            getYelpData(marker.creamery, marker.address);
             infowindow.open(map, marker);
-
-
             infowindow.addListener('closeclick', function() {
                 infowindow.marker = null;
                 marker.setIcon(defaultIconColor);
@@ -265,6 +269,71 @@ var ViewModel = function() {
 
     }
 };
+
+
+var getYelpData = function(name, addr){
+  // Uses the oauth-signature package installed with bower per https://github.com/bettiolo/oauth-signature-js
+
+    // Use the GET method for the request
+    var httpMethod = 'GET';
+
+    // Yelp API request url
+    var yelpURL = 'http://api.yelp.com/v2/search/';
+
+    // nonce generator
+    // function credit of: https://blog.nraboy.com/2015/03/create-a-random-nonce-string-using-javascript/
+    var nonce = function(length) {
+        var text = "";
+        var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        for(var i = 0; i < length; i++) {
+            text += possible.charAt(Math.floor(Math.random() * possible.length));
+        }
+        return text;
+    };
+
+    // Set required parameters for authentication & search
+    var parameters = {
+      oauth_consumer_key: 'S46AQ1iwQtvxw_D1wQLHZA',
+      oauth_token: 'TO9rPx1abdPe3lllR5Wo3WFrvz8CV9vw',
+      oauth_nonce: nonce(20),
+      oauth_timestamp: Math.floor(Date.now() / 1000),
+      oauth_signature_method: 'HMAC-SHA1',
+      oauth_version: '1.0',
+      callback: 'cb',
+      term: name,
+      location: addr,
+      limit: 1
+    };
+
+    // Set other API parameters
+    var consumerSecret = '8hqIHpplfRBLzs6YOqLZFfkx7jg';
+    var tokenSecret = 'evb3bjTox8RNlfZ5Ma74hqJjZWo';
+
+    // generates a RFC 3986 encoded, BASE64 encoded HMAC-SHA1 hash
+    var signature = oauthSignature.generate(httpMethod, yelpURL, parameters, consumerSecret, tokenSecret);
+
+    // Add signature to list of parameters
+    parameters.oauth_signature = signature;
+
+    // Set up the ajax settings
+    var ajaxSettings = {
+      url: yelpURL,
+      data: parameters,
+      cache: true,
+      dataType: 'jsonp',
+      success: function(response) {
+        // Update the infoWindow to display the yelp rating image
+        $('#yelp').attr("src", response.businesses[0].rating_img_url);
+        $('#yelp-url').attr("href", response.businesses[0].url);
+      },
+      error: function() {
+        $('#text').html('Data could not be retrieved from yelp.');
+      }
+    };
+
+    // Send off the ajaz request to Yelp
+    $.ajax(ajaxSettings);
+}
 
 var mapsInitError = function() {
     alert("Google Maps failed to load");
