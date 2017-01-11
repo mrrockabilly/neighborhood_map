ace3558ad4504912261fe21563f5408706c47f67
jrrubalc <jason.ray.rubalcaba@gmail.com>
2017-01-10 20:15:54 -0800

    added locations and syling

--- js/places.js
+++ js/places.js
@@ -20,6 +20,62 @@ var iceCreamLocations = [{
         "lat": 34.062088,
         "lng": -118.347813
     }
+},
+{
+    creamery: "Magnoila Bakery",
+    iceCreamLocation: "8389 W 3rd St, Los Angeles, CA 90048",
+    coordinates: {
+        "lat": 34.073152,
+        "lng": -118.372998
+    }
+},
+{
+    creamery: "Ice Cream Lab",
+    iceCreamLocation: "369 E 2nd St, Los Angeles, CA 90012",
+    coordinates: {
+        "lat": 34.048301,
+        "lng": -118.239929
+    }
+},
+{
+    creamery: "Sprinkles Ice Cream",
+    iceCreamLocation: "9631 S Santa Monica Blvd, Beverly Hills, CA 90210",
+    coordinates: {
+        "lat": 34.069642,
+        "lng": -118.405989
+    }
+},
+{
+    creamery: "Fosselman's Ice Cream Co.",
+    iceCreamLocation: "1824 W Main St, Alhambra, CA 91801",
+    coordinates: {
+        "lat": 34.091294,
+        "lng": -118.143880
+    }
+},
+{
+    creamery: "Mateo's Ice Cream & Fruit Bars",
+    iceCreamLocation: "1250 S Vermont Ave, Los Angeles, CA 90006",
+    coordinates: {
+        "lat": 34.047658,
+        "lng": -118.291205
+    }
+},
+{
+    creamery: "Sweet Rose Creamery",
+    iceCreamLocation: "7565 Beverly Blvd, Los Angeles, CA 90036",
+    coordinates: {
+        "lat": 34.076369,
+        "lng": -118.354860
+    }
+},
+{
+    creamery: "Creamistry",
+    iceCreamLocation: "3510 W 8th St, Los Angeles, CA 90005",
+    coordinates: {
+        "lat": 34.057424,
+        "lng": -118.307605
+    }
 }
 ];
 
